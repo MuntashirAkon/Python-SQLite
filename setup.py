9d288a9ceba846cc7acd59d6a0a0b6b75690c454
Muntashir Al-Islam <muntashir.islam96@gmail.com>
2015-10-22 17:10:15 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from distutils.core import setup
+
+setup(name='SQLite',
+      version='0.1.0',
+      description='SQLite utilities with a PHP-MySQLi like syntax',
+      author='Muntashir Al-Islam',
+      author_email='muntashir.islam96@gmail.com',
+      url='https://github.com/MuntashirAkon/Python-SQLite',
+      packages=['sqlite'],
+     )
